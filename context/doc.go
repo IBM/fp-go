308fe487189ed307524ccfce70eb89adcf2446dd
Dr. Carsten Leue <carsten.leue@de.ibm.com>
2023-07-18 15:45:02 +0200

    doc: try package doc
    Signed-off-by: Dr. Carsten Leue <carsten.leue@de.ibm.com>

--- context/doc.go
+++ context/doc.go
@@ -12,4 +12,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+// Package context contains versions of monads that work with a golang [context.Context]
 package context
