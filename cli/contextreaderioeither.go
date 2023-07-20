ff7d750d978e9db883fe470eb936160568981cdd
Dr. Carsten Leue <carsten.leue@de.ibm.com>
2023-07-20 23:22:02 +0200

    fix: auto generate SequenceTuple for ReaderIOEither
    Signed-off-by: Dr. Carsten Leue <carsten.leue@de.ibm.com>

--- cli/contextreaderioeither.go
+++ cli/contextreaderioeither.go
@@ -25,6 +25,88 @@ func generateNestedCallbacks(i, total int) string {
 	return buf.String()
 }
 
+func generateContextReaderIOEitherSequenceTuple(f, fg *os.File, i int) {
+	// tuple type
+	tuple := tupleType(i)
+
+	// non-generic version
+	// generic version
+	fmt.Fprintf(f, "\n// SequenceTuple%d converts a [T.Tuple%d] of [ReaderIOEither] into a [ReaderIOEither] of a [T.Tuple%d].\n", i, i, i)
+	fmt.Fprintf(f, "func SequenceTuple%d[", i)
+	for j := 0; j < i; j++ {
+		if j > 0 {
+			fmt.Fprintf(f, ", ")
+		}
+		fmt.Fprintf(f, "T%d", j+1)
+	}
+	fmt.Fprintf(f, " any](t T.Tuple%d[", i)
+	for j := 0; j < i; j++ {
+		if j > 0 {
+			fmt.Fprintf(f, ", ")
+		}
+		fmt.Fprintf(f, "ReaderIOEither[T%d]", j+1)
+	}
+	fmt.Fprintf(f, "]) ReaderIOEither[%s] {\n", tuple)
+	fmt.Fprintf(f, "  return G.SequenceTuple%d[ReaderIOEither[%s]](t)\n", i, tuple)
+	fmt.Fprintf(f, "}\n")
+
+	// generic version
+	fmt.Fprintf(fg, "\n// SequenceTuple%d converts a [T.Tuple%d] of readers into a reader of a [T.Tuple%d].\n", i, i, i)
+	fmt.Fprintf(fg, "func SequenceTuple%d[\n", i)
+
+	fmt.Fprintf(fg, "  GR_TUPLE%d ~func(context.Context) GIO_TUPLE%d,\n", i, i)
+	for j := 0; j < i; j++ {
+		fmt.Fprintf(fg, "  GR_T%d ~func(context.Context) GIO_T%d,\n", j+1, j+1)
+	}
+
+	fmt.Fprintf(fg, "  GIO_TUPLE%d ~func() E.Either[error, %s],\n", i, tuple)
+	for j := 0; j < i; j++ {
+		fmt.Fprintf(fg, "  GIO_T%d ~func() E.Either[error, T%d],\n", j+1, j+1)
+	}
+
+	for j := 0; j < i; j++ {
+		fmt.Fprintf(fg, "  T%d", j+1)
+		if j < i-1 {
+			fmt.Fprintf(fg, ",\n")
+		}
+	}
+	fmt.Fprintf(fg, " any](t T.Tuple%d[", i)
+	for j := 0; j < i; j++ {
+		if j > 0 {
+			fmt.Fprintf(fg, ", ")
+		}
+		fmt.Fprintf(fg, "GR_T%d", j+1)
+	}
+	fmt.Fprintf(fg, "]) GR_TUPLE%d {\n", i)
+	fmt.Fprintf(fg, "  return A.SequenceTuple%d(\n", i)
+	// map call
+	var cr string
+	if i > 1 {
+		cb := generateNestedCallbacks(1, i)
+		cio := fmt.Sprintf("func() E.Either[error, %s]", cb)
+		cr = fmt.Sprintf("func(context.Context) %s", cio)
+	} else {
+		cr = fmt.Sprintf("GR_TUPLE%d", i)
+	}
+	fmt.Fprintf(fg, "    Map[GR_T%d, %s, GIO_T%d],\n", 1, cr, 1)
+	// the apply calls
+	for j := 1; j < i; j++ {
+		if j < i-1 {
+			cb := generateNestedCallbacks(j+1, i)
+			cio := fmt.Sprintf("func() E.Either[error, %s]", cb)
+			cr = fmt.Sprintf("func(context.Context) %s", cio)
+		} else {
+			cr = fmt.Sprintf("GR_TUPLE%d", i)
+		}
+		fmt.Fprintf(fg, "    Ap[%s, func(context.Context) func() E.Either[error, %s], GR_T%d],\n", cr, generateNestedCallbacks(j, i), j+1)
+	}
+	// raw parameters
+	fmt.Fprintf(fg, "    t,\n")
+
+	fmt.Fprintf(fg, " )\n")
+	fmt.Fprintf(fg, "}\n")
+}
+
 func generateContextReaderIOEitherSequenceT(f, fg *os.File, i int) {
 	// tuple type
 	tuple := tupleType(i)
@@ -108,11 +190,7 @@ func generateContextReaderIOEitherSequenceT(f, fg *os.File, i int) {
 	for j := 0; j < i; j++ {
 		fmt.Fprintf(fg, "    t%d,\n", j+1)
 	}
-	// // map
-	// 	Ap[GB, func(E) func() ET.Either[L, func(C) T.Tuple3[A, B, C]], func(E) func() ET.Either[L, func(B) func(C) T.Tuple3[A, B, C]], GIOB, func() ET.Either[L, func(C) T.Tuple3[A, B, C]], func() ET.Either[L, func(B) func(C) T.Tuple3[A, B, C]], E, L, B, func(C) T.Tuple3[A, B, C]],
-	// 	Ap[GC, GTABC, func(E) func() ET.Either[L, func(C) T.Tuple3[A, B, C]], GIOC, GIOTABC, func() ET.Either[L, func(C) T.Tuple3[A, B, C]], E, L, C, T.Tuple3[A, B, C]],
 
-	// 	a, b, c,
 	fmt.Fprintf(fg, " )\n")
 	fmt.Fprintf(fg, "}\n")
 }
@@ -222,6 +300,8 @@ import (
 		generateContextReaderIOEitherEitherize(f, fg, i)
 		// sequenceT
 		generateContextReaderIOEitherSequenceT(f, fg, i)
+		// sequenceTuple
+		generateContextReaderIOEitherSequenceTuple(f, fg, i)
 	}
 
 	return nil
