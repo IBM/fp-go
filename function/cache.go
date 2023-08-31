80e579dd0bb6109b75653e30f712bce138469305
Dr. Carsten Leue <carsten.leue@de.ibm.com>
2023-08-31 10:27:32 +0200

    fix: implement simple cache for pure functions
    Signed-off-by: Dr. Carsten Leue <carsten.leue@de.ibm.com>

---
+++ function/cache.go
@@ -0,0 +1,30 @@
+// Copyright (c) 2023 IBM Corp.
+// All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package function
+
+import (
+	G "github.com/IBM/fp-go/function/generic"
+)
+
+// Cache converts a unary function into a unary function that caches the value depending on the parameter
+func Cache[K comparable, T any](f func(K) T) func(K) T {
+	return G.Cache(f)
+}
+
+// ContramapCache converts a unary function into a unary function that caches the value depending on the parameter
+func ContramapCache[A any, K comparable, T any](kf func(A) K) func(func(A) T) func(A) T {
+	return G.ContramapCache[func(A) T](kf)
+}
